26416fa025956fe267952c828b437e2f12fd2f5e
Mike Kenyon <mike.kenyon@gmail.com>
2011-03-13 23:01:22 -0700

    add docstrings

--- test/restlibtestlive.py
+++ test/restlibtestlive.py
@@ -1,17 +1,43 @@
+"""Unit tests for restlib, using a live source.
+
+   Tests in this suite are not meant to thoroughly test restlib because
+   we do not control the data that is received. To thoroughly test
+   restlib, we have the restlibtestmock suite.
+   
+   Rather, this suite is designed to ensure that restlib does actually
+   work in "real work" conditions.
+   
+   It does feel wrong using others' resources for testing purposes,
+   but given the magnitude of page hits that reddit receives,
+   this test will be a drop in the ocean."""
+   
+__license__ = 'Python'
+__author__ = 'Mike Kenyon <mike.kenyon@gmail.com'
+__version__ = 0.1 
+
 import restlib
 import unittest
 import urlparse
 
 class RestLibTestLive(unittest.TestCase):
+
     def testGETNoScheme(self):
-        rest = restlib.RestLib('www.reddit.com')       
+        """Ensure that GET works when used without a protocol."""
+        rest = restlib.RestService('www.reddit.com')       
         responseObj = rest.request_get('/user/sure_ill_draw_that/.json')
+        #print responseObj
+    
     def testGETWithScheme(self):
-        rest = restlib.RestLib('http://www.reddit.com')
+        """Ensure that GET works when used with a protocol."""
+        rest = restlib.RestService('http://www.reddit.com')
         responseObj = rest.request_get('/user/sure_ill_draw_that/.json')
+        #print responseObj
+    
     def testGETQuery(self):
-        rest= restlib.RestLib('www.reddit.com')
+        """Ensure that GET works when given a query."""
+        rest= restlib.RestService('www.reddit.com')
         resource = '/user/sure_ill_draw_that/.json'
         responseObjControversial = rest.request_get(resource, {'sort': 'controversial'})
         responseObjTop = rest.request_get(resource, {'sort': 'top'})
         self.assertNotEqual(responseObjControversial, responseObjTop, 'controversial should not equal top!')
+        #print responseObj
--- test/restlibtestmock.py
+++ test/restlibtestmock.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-"""Unit test for restlib"""
+"""Unit tests for restlib, using a mock framework"""
 
 __license__ = 'Python'
 __author__ = 'Mike Kenyon <mike.kenyon@gmail.com'
@@ -15,21 +13,21 @@ import unittest
 class GoodConnection(unittest.TestCase):
     def testGET(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r)       
         for k, v in r.GET.iteritems():
             responseObj = rest.request_get(k)
             self.assertEquals(responseObj, v[1])
     def testGETArgs(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r)
         for k, v in r.ARGS.iteritems():
             responseObj = rest.request_get("/has/args", args=eval(k))
             self.assertEquals(responseObj, v)
     def testPOST(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r)
         for k, v in r.POST.iteritems():
             responseObj = rest.request_post(k, body = v[2], args = v[3])
@@ -37,65 +35,65 @@ class GoodConnection(unittest.TestCase):
 class BadInput(unittest.TestCase):
     def testNotFound(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r)
         self.assertRaises(restlib.HTTPException, rest.request_get, '/file_not_found')
         
     def testBadRequest(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r)
         self.assertRaises(restlib.HTTPException, rest.request_get, '/400')
         
     def testForbidden(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r)
         self.assertRaises(restlib.HTTPException, rest.request_get, '/403') 
         
     def testNotFoundPOST(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r)
         self.assertRaises(restlib.HTTPException, rest.request_post, '/file_not_found')
         
     def testBadRequestPOST(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r)
         self.assertRaises(restlib.HTTPException, rest.request_post, '/400')
         
     def testForbiddenPOST(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r)
         self.assertRaises(restlib.HTTPException, rest.request_post, '/403') 
         
 class BadServerResponse(unittest.TestCase):
     def testUnterminatedJSON(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r) 
         self.assertRaises(restlib.JSONException, rest.request, "/unterminatedJSON", verb="GET_TEST")
     def testOverterminatedJSON(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r) 
         self.assertRaises(restlib.JSONException, rest.request, "/overterminatedJSON", verb="GET_TEST")
     def testNoJSON(self):
         r = Responses()
-        rest = restlib.RestLib('http://www.example.com')
+        rest = restlib.RestService('http://www.example.com')
         rest.conn = MockHTTPConnection(r) 
         self.assertRaises(restlib.JSONException, rest.request, "/noJSON", verb="GET_TEST")
         
 class SSL(unittest.TestCase):
     def testUseSSLByParam(self):
-        rest = restlib.RestLib('https://www.example.com', secure=True)
+        rest = restlib.RestService('https://www.example.com', secure=True)
         self.assertTrue(isinstance(rest.conn, httplib.HTTPSConnection), "SSL by param does not use HTTPSConnection")
     def testDoNotUseSSLByParam(self):
-        rest = restlib.RestLib('http://www.example.com', secure=False)
+        rest = restlib.RestService('http://www.example.com', secure=False)
         self.assertFalse(isinstance(rest.conn, httplib.HTTPSConnection), "No SSL by param uses HTTPSConnection")
     def testConfuseSSLYesParamNoScheme(self):
-        self.assertRaises(ValueError, restlib.RestLib, 'http://www.example.com', secure=True)
+        self.assertRaises(ValueError, restlib.RestService, 'http://www.example.com', secure=True)
     def testConfuseSSLNoParamYesScheme(self):
-        self.assertRaises(ValueError, restlib.RestLib, 'https://www.example.com', secure=False)
+        self.assertRaises(ValueError, restlib.RestService, 'https://www.example.com', secure=False)
