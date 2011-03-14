514ee2fb5bca549a1f5123449b1743c9b6f44255
Mike Kenyon <mike.kenyon@gmail.com>
2011-03-13 18:43:23 -0700

    add explanatory comments to the server responses file, Responses.py

--- test/Responses.py
+++ test/Responses.py
@@ -1,6 +1,9 @@
 class Responses:
     def __init__(self):
         self.GET = dict()
+        # key is the path to the resource
+        # v[0] is the server response for the given resource
+        # v[1] is the appropriate python object for the response
         self.GET['/'] = ('{ "isRoot": true }', {'isRoot': True})
         self.GET['/widget'] = ('{"widget":{"debug": "on","window": { "title": "Sample Konfabulator Widget",        "name": "main_window",        "width": 500,        "height": 500    },    "image": {        "src": "Images/Sun.png",        "name": "sun1",        "hOffset": 250,        "vOffset": 250,        "alignment": "center"    },    "text": {        "data": "Click Here",        "size": 36,        "style": "bold",        "name": "text1",        "hOffset": 250,        "vOffset": 100,        "alignment": "center",        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"    }}}    '
                                , {'widget': {'debug': 'on', 'text': {'vOffset': 100, 'style': 'bold', 'name': 'text1', 'hOffset': 250, 'onMouseUp': 'sun1.opacity = (sun1.opacity / 100) * 90;','data': 'Click Here', 'alignment': 'center', 'size': 36}, 'window': {'width': 500, 'height': 500, 'name': 'main_window', 'title': 'Sample Konfabulator Widget'}, 'image': {'vOffset': 250, 'src': 'Images/Sun.png', 'alignment': 'center', 'name': 'sun1', 'hOffset': 250}}}
@@ -12,13 +15,23 @@ class Responses:
                                               , {'menu': {'header': 'SVG Viewer', 'items': [{'id': 'Open'}, {'id': 'OpenNew', 'label': 'Open New'}, None, {'id': 'ZoomIn', 'label': 'Zoom In'}, {'id': 'ZoomOu t', 'label': 'Zoom Out'}, {'id': 'OriginalView', 'label': 'Original View'}, None, {'id': 'Quality'}, {'id': 'Pause'}, {'id': 'Mute'}, None, {'id': 'Find', 'label': 'Find...'}, {'id': 'FindAgain', 'label': 'Find Again'}, {'id': 'Copy'}, {'id': 'CopyAgain', 'label': 'Copy Again'}, {'id': 'CopySVG', 'label': 'Copy SVG'}, {'id': 'ViewSVG', 'label': 'View SVG'}, {'id': 'ViewSource', 'label': 'View Source'}, {'id': 'SaveAs', 'label': 'Save As'}, None, {'id': 'Help'}, {'id': 'About', 'label': 'About Adobe CVG Viewer...'}]}}                                           
                                               )
         
+        # Ensuring that restlib accepts key-value dictionaries
+        # for the URL parameters
+        # k = dictionary passed by client
+        # v = the appropriate python object for the server's response
+        # The server's response is stored in the following GET dictionary.
         self.ARGS = dict()
         self.ARGS['{ "one" : True }'] = {'args': {'one': True}}
         self.ARGS['{"one":True, "two":3}'] = {'args': {'two': 3, 'one': True}}
+        # Note that these will be run during plain old GET testing, too.
         self.GET['/has/args?one=True'] = ('{ "args": { "one" : true } }', {'args': {'one': True}})
         self.GET['/has/args?one=True&two=3'] = ('{"args":{"one":true, "two":3}}', {'args': {'two': 3, 'one': True}})
         self.GET['/has/args?two=3&one=True'] = ('{"args":{"one":true, "two":3}}', {'args': {'two': 3, 'one': True}})
         
+        # Poorly formatted server responses for GET queries go here.
+        # We use a separate dictionary because these should fail
+        # and failing cases shouldn't run in the same test as passing
+        # cases. 
         self.GET_TEST = dict()
         self.GET_TEST['/unterminatedJSON'] = ('{', )
         self.GET_TEST['/overterminatedJSON'] = ('{ "isRoot": true }}', )
